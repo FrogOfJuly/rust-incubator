76c2484d3a0adddf1292aec572b06cf6e8bf0ddb
Kirill Golubev <frogofjuly@Me.local>
2023-09-06 15:54:59 +0300

    cow email implementation

--- 1_concepts/1_5_convert_cast_deref/src/main.rs
+++ 1_concepts/1_5_convert_cast_deref/src/main.rs
@@ -1,60 +1,92 @@
-use std::ops::{Deref, DerefMut};
+use std::{
+    borrow::Cow,
+    ops::{Deref, DerefMut},
+};
 
 #[allow(dead_code)]
 struct EmailError {
     cause: String,
 }
 
-struct EmailString(String);
+struct EmailString<'a>(Cow<'a, str>);
 
-impl EmailString {
-    fn new(email: String) -> Self {
+impl<'a> EmailString<'a> {
+    fn new(email: Cow<'a, str>) -> Self {
         EmailString(email)
     }
-}
 
-impl TryFrom<String> for EmailString {
-    type Error = EmailError;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        let parts = value.split('@').collect::<Vec<_>>();
-        let [left, right] = parts.as_slice() else {
-            return Err(EmailError {
-                cause: "Only one @ permitted".to_owned(),
+    fn check_email(value: &str) -> Option<EmailError> {
+        let mut parts = value.split('@');
+        let Some(left) = parts.next() else {
+            return Some(EmailError {
+                cause: "Exactly one @ is required".to_owned(),
             });
         };
 
+        let Some(right) = parts.next() else {
+            return Some(EmailError {
+                cause: "Exactly one @ is required".to_owned(),
+            });
+        };
+
+        if parts.count() != 0 {
+            return Some(EmailError {
+                cause: "Exactly one @ is required".to_owned(),
+            });
+        }
+
         if left.is_empty() || right.is_empty() {
-            return Err(EmailError {
+            return Some(EmailError {
                 cause: "@ can't be at the start or the end of an email".to_owned(),
             });
         }
 
         if !right.contains('.') {
-            return Err(EmailError {
+            return Some(EmailError {
                 cause: "Domen must be specified after @".to_owned(),
             });
         }
-
-        Ok(EmailString::new(value))
+        None
     }
 }
 
-impl From<EmailString> for String {
+impl<'a> TryFrom<String> for EmailString<'a> {
+    type Error = EmailError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        if let Some(err) = Self::check_email(value.as_str()) {
+            return Err(err);
+        }
+        Ok(EmailString(Cow::Owned(value)))
+    }
+}
+
+impl<'a> TryFrom<&'a str> for EmailString<'a> {
+    type Error = EmailError;
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        if let Some(err) = Self::check_email(value) {
+            return Err(err);
+        }
+        Ok(EmailString::new(Cow::Borrowed(value)))
+    }
+}
+
+impl<'a> From<EmailString<'a>> for String {
     fn from(val: EmailString) -> Self {
-        val.0
+        val.0.into_owned()
     }
 }
 
-impl AsRef<str> for EmailString {
+impl<'a> AsRef<str> for EmailString<'a> {
     fn as_ref(&self) -> &str {
         self.0.as_ref()
     }
 }
 
-impl AsMut<str> for EmailString {
+impl<'a> AsMut<str> for EmailString<'a> {
     fn as_mut(&mut self) -> &mut str {
-        self.0.as_mut_str()
+        self.0.to_mut()
     }
 }
 
