3b6f5c5f69047485d84f323a8ce9cbd406c09ae3
Kirill Golubev <frogofjuly@Me.local>
2023-09-06 17:55:08 +0300

    undo cut

--- 1_concepts/1_6_dispatch/src/main.rs
+++ 1_concepts/1_6_dispatch/src/main.rs
@@ -7,7 +7,6 @@ trait Storage<K, V> {
     fn get(&self, key: &K) -> Option<&V>;
     fn remove(&mut self, key: &K) -> Option<V>;
 }
-trait UserStorage = Storage<String, User>;
 
 struct User {
     id: u64,
@@ -15,7 +14,7 @@ struct User {
     activated: bool,
 }
 
-
+trait UserStorage = Storage<String, User>;
 
 mod static_dispatch {
     use crate::{UserStorage, User, Storage};
@@ -28,6 +27,33 @@ mod dynamic_dispatch {
 
     use crate::{UserStorage, User, Storage};
 
+    struct UserRepository {
+        storage: Box<dyn UserStorage>,
+    }
+
+    impl UserRepository {
+        pub fn new(storage: Box<dyn UserStorage>) -> Self {
+            Self { storage }
+        }
+
+        pub fn replace_storage(&mut self, storage: Box<dyn UserStorage>) -> Box<dyn UserStorage> {
+            replace(&mut self.storage, storage)
+        }
+    }
+
+    impl Storage<String, User> for UserRepository{
+        fn set(&mut self, key: String, val: User) {
+            self.storage.set(key, val)
+        }
+
+        fn get(&self, key: &String) -> Option<&User> {
+            self.storage.get(key)
+        }
+
+        fn remove(&mut self, key: &String) -> Option<User> {
+            self.storage.remove(key)
+        }
+    }
 }
 
 fn main() {
