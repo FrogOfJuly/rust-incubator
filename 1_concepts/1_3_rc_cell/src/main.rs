475b98c8f78ec4835dd98b001023b142e1ef0df4
Kirill Golubev <frogofjuly@Me.local>
2023-09-06 15:28:35 +0300

    current progress for 1st task

--- 1_concepts/1_3_rc_cell/src/main.rs
+++ 1_concepts/1_3_rc_cell/src/main.rs
@@ -1,3 +1,66 @@
+mod global_stack {
+    use std::{cell::RefCell, rc::Rc};
+
+    #[derive(Clone, Debug)]
+    pub struct GlobalStack<T> {
+        instance: Rc<RefCell<Stack<T>>>,
+    }
+
+    impl<T> Default for GlobalStack<T> {
+        fn default() -> Self {
+            Self {
+                instance: Default::default(),
+            }
+        }
+    }
+
+    impl<T> GlobalStack<T> {
+        pub fn new() -> GlobalStack<T> {
+            GlobalStack::default()
+        }
+
+        pub fn pop(&self) -> Option<T> {
+            self.instance.as_ref().borrow_mut().pop()
+        }
+
+        pub fn push(&self, v: T) {
+            self.instance.as_ref().borrow_mut().push(v)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Stack<T> {
+        storage: Vec<T>,
+    }
+
+    impl<T> Default for Stack<T> {
+        fn default() -> Stack<T> {
+            Self { storage: vec![] }
+        }
+    }
+
+    impl<T> Stack<T> {
+        fn pop(&mut self) -> Option<T> {
+            self.storage.pop()
+        }
+
+        fn push(&mut self, v: T) {
+            self.storage.push(v)
+        }
+    }
+}
 fn main() {
-    println!("Implement me!");
+    let stack = global_stack::GlobalStack::<u32>::new();
+
+    let stack_clone = stack.clone();
+
+    [1, 2, 3].into_iter().for_each(|x| stack.push(x));
+
+    println!("{:?}", stack_clone);
+
+    [1, 2, 3].into_iter().for_each(|_| {
+        stack.pop();
+    });
+
+    println!("{:?}", stack_clone);
 }
--- 1_concepts/1_5_convert_cast_deref/src/main.rs
+++ 1_concepts/1_5_convert_cast_deref/src/main.rs
@@ -1,3 +1,119 @@
+use std::ops::{Deref, DerefMut};
+
+#[allow(dead_code)]
+struct EmailError {
+    cause: String,
+}
+
+struct EmailString(String);
+
+impl EmailString {
+    fn new(email: String) -> Self {
+        EmailString(email)
+    }
+}
+
+impl TryFrom<String> for EmailString {
+    type Error = EmailError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let parts = value.split('@').collect::<Vec<_>>();
+        let [left, right] = parts.as_slice() else {
+            return Err(EmailError {
+                cause: "Only one @ permitted".to_owned(),
+            });
+        };
+
+        if left.is_empty() || right.is_empty() {
+            return Err(EmailError {
+                cause: "@ can't be at the start or the end of an email".to_owned(),
+            });
+        }
+
+        if !right.contains('.') {
+            return Err(EmailError {
+                cause: "Domen must be specified after @".to_owned(),
+            });
+        }
+
+        Ok(EmailString::new(value))
+    }
+}
+
+impl From<EmailString> for String {
+    fn from(val: EmailString) -> Self {
+        val.0
+    }
+}
+
+impl AsRef<str> for EmailString {
+    fn as_ref(&self) -> &str {
+        self.0.as_ref()
+    }
+}
+
+impl AsMut<str> for EmailString {
+    fn as_mut(&mut self) -> &mut str {
+        self.0.as_mut_str()
+    }
+}
+
+// impl Deref for EmailString {
+//     type Target = str;
+
+//     fn deref(&self) -> &Self::Target {
+//         self.0.as_ref()
+//     }
+// }
+
+struct Random<T>(T, T, T);
+
+impl<T> Random<T> {
+    fn choose(&self) -> &T {
+        match rand::random::<usize>() % 3 {
+            0 => &self.0,
+            1 => &self.1,
+            2 => &self.2,
+            _ => unreachable!(),
+        }
+    }
+
+    fn choose_mut(&mut self) -> &mut T {
+        match rand::random::<usize>() % 3 {
+            0 => &mut self.0,
+            1 => &mut self.1,
+            2 => &mut self.2,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl<T> Deref for Random<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.choose()
+    }
+}
+
+impl<T> DerefMut for Random<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.choose_mut()
+    }
+}
+
+// impl<T> AsRef<T> for Random<T> {
+//     fn as_ref(&self) -> &T {
+//         self.choose()
+//     }
+// }
+
+// impl<T> AsMut<T> for Random<T> {
+//     fn as_mut(&mut self) -> &mut T {
+//         self.choose_mut()
+//     }
+// }
+
 fn main() {
     println!("Implement me!");
 }
